05f0291525f193b3848e3bd74627084752cc885f
Marien Fressinaud <dev@marienfressinaud.fr>
2013-05-29 23:31:13 +0200

    Début formulaire de connexion

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,30 @@
 package controllers;
 
 import models.*;
+import play.data.Form;
+import static play.data.Form.*;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
-  
+	public static class Login {
+		public String pseudo;
+		public String password;
+	}
+	
 	public static Result index() {
 		return ok(index.render(
 			Joueur.listByScore()
 		));
 	}
-  
+	
+	public static Result login() {
+		return ok(login.render(form(Login.class)));
+	}
+	
+	public static Result authenticate() {
+		Form<Login> loginForm = form(Login.class).bindFromRequest();
+		return ok();
+	}
 }
